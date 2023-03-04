fca37b1163bbc99447ce7061a1261101a2d12b66
Junior Silva <jr.silva-@hotmail.com>
2023-03-03 21:26:28 -0300

    Adicionada funcionalidade de atack, e melhorias nas configurações.

--- scriptBotTibia.py
+++ scriptBotTibia.py
@@ -7,6 +7,11 @@ import threading
 
 #Configurações de cura do personagem
 config = {
+    'healMode':True,
+    'manaMode':False,
+    'foodMode':True,
+    'unlogoutMode':True,
+    'attackMode':True,
     'timeHeal': 1.2,
     'timeHealDanger': 1,
     'timeHealMana': 2.0,
@@ -20,7 +25,7 @@ path = "C:/Users/junior.silva/AppData/Local/Tesseract-OCR/tesseract.exe"
 pytesseract.pytesseract.tesseract_cmd = path
 
 #Faz a leitura dos dados
-def convertString(currentScreen):
+def convertToInt(currentScreen):
     currentValue = cv2.cvtColor(np.array(currentScreen), cv2.COLOR_RGB2BGR)
     try:
         return int(pytesseract.image_to_string(currentValue))
@@ -30,12 +35,12 @@ def convertString(currentScreen):
 #Captura screenshot da vida do personagem
 def getLife():
     currentScreen = pyautogui.screenshot(region=(1864, 138, 45, 18))
-    return convertString(currentScreen)
+    return convertToInt(currentScreen)
 
 #Captura screenshot da mana do personagem
 def getMana():
     currentScreen = pyautogui.screenshot(region=(1864, 152, 45, 18))
-    return convertString(currentScreen)
+    return convertToInt(currentScreen)
 
 #Garante que o personagem se mantenha logado
 def unlogout():
@@ -68,9 +73,33 @@ def healMana():
             pyautogui.hotkey('F8')
             sleep(config['timeHealMana'])
 
+def findMonster():
+    currentScreen = pyautogui.screenshot(region=(1748, 415, 154, 27))
+    currentValue = cv2.cvtColor(np.array(currentScreen), cv2.COLOR_RGB2BGR)
+    try:
+        monstro = str(pytesseract.image_to_string(currentValue))
+        if len(monstro) > 0:
+            return True
+    except:
+        return False
+
+#Ataca os montros
+def attack():
+    while True:
+        if findMonster():
+            #pyautogui.click(x=1811, y=428)
+            pyautogui.hotkey("F2")
+            sleep(2)
+
 #Realiza a execução das tarefas em paralelo
 if __name__ == "__main__":
-    threading.Thread(target=eatFood).start()
-    threading.Thread(target=unlogout).start()
-    threading.Thread(target=healLife).start()
-    threading.Thread(target=healMana).start()
+    if config['foodMode']:
+        threading.Thread(target=eatFood).start()
+    if config['unlogoutMode']:
+        threading.Thread(target=unlogout).start()
+    if config['healMode']:
+        threading.Thread(target=healLife).start()
+    if config['manaMode']:
+        threading.Thread(target=healMana).start()
+    if config['attackMode']:
+        threading.Thread(target=attack).start()
